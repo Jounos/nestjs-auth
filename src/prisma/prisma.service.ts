c51dc35179bcaf06a09ca53ad5db7360ef42e6ac
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-04-18 01:35:12 -0300

    Correção do nome do projeto em ambiente local

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,4 +2,9 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        await this.$connect();
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    constructor(private prismaService: PrismaService) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    create(createUserDto: CreateUserDto) {
+        return 'This action adds a new user';
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    findAll() {
+        return `This action returns all users`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} user`;
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
 }

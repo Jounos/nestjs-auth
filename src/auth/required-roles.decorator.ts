ecb5b82d548a9304815de66f07893317bf6347c3
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-04-29 18:46:22 -0300

    Implementado autorizaçã

---
+++ src/auth/required-roles.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from "@nestjs/common";
+import { Roles } from "generated/prisma";
+
+export const RequiredRoles = (...roles: Roles[]) => SetMetadata('roles', roles);
+
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { Roles } from 'generated/prisma';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RequiredRoles } from 'src/auth/required-roles.decorator';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-import { Request } from 'express';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { PostsService } from './posts.service';
+import { RoleGuard } from 'src/auth/role/role.guard';
 
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postsService: PostsService) { }
 
     @Post()
+    @RequiredRoles(Roles.WRITER, Roles.EDITOR)
     create(@Body() createPostDto: CreatePostDto, @Req() req: Request) {
         return this.postsService.create({
             ...createPostDto,
@@ -19,21 +23,25 @@ export class PostsController {
     }
 
     @Get()
+    @RequiredRoles(Roles.WRITER, Roles.EDITOR, Roles.EDITOR)
     findAll() {
         return this.postsService.findAll();
     }
 
     @Get(':id')
+    @RequiredRoles(Roles.WRITER, Roles.EDITOR, Roles.EDITOR)
     findOne(@Param('id') id: string) {
         return this.postsService.findOne(id);
     }
 
     @Patch(':id')
+    @RequiredRoles(Roles.WRITER, Roles.EDITOR)
     update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
         return this.postsService.update(id, updatePostDto);
     }
 
     @Delete(':id')
+    @RequiredRoles(Roles.ADMIN)
     remove(@Param('id') id: string) {
         return this.postsService.remove(id);
     }
